6d160142b85d1c07ba877007f75b43c6ad1f81e8
sloik89 <sloik89@gmail.com>
2023-08-21 22:49:37 +0200

    second verion of router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import { TopLeftImg, Nav, Header } from "./components";
 
 import { AnimatePresence, motion } from "framer-motion";
@@ -9,19 +9,17 @@ import { Transition } from "./components";
 function App() {
   return (
     <div className="page">
-      <BrowserRouter>
-        <TopLeftImg />
-        <Nav />
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/services" element={<Services />} />
-          <Route path="/work" element={<Work />} />
-          <Route path="/testimonials" element={<Testimonials />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-      </BrowserRouter>
+      <TopLeftImg />
+      <Nav />
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/work" element={<Work />} />
+        <Route path="/testimonials" element={<Testimonials />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </div>
   );
 }
